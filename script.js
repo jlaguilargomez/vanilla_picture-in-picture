b7b2e6af8d32e5d85364795c032a99bca5758107
jlaguilargomez <jlaguigom@gmail.com>
2020-10-07 19:45:32 +0200

    Project finished with new personal features

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log('testing!')
+const videoElement = document.getElementById('video');
+const buttonCapture = document.getElementById('button-capture')
+const buttonLaunch = document.getElementById('button-launch')
+
+// Prompt to select media stream, pass to video element, then play
+async function selectMediaStream() {
+  try { 
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    }
+  } catch (err) {
+    console.log('Fail!', err)
+  }
+}
+
+buttonCapture.addEventListener('click', () => {
+  selectMediaStream();
+})
+
+buttonLaunch.addEventListener('click', async () => {
+  // Disable Button
+  buttonLaunch.disabled = true;
+
+  // Start picture in picture
+  await videoElement.requestPictureInPicture();
+
+  // Reset Button
+  buttonLaunch.disabled = false;
+})
+
+// On load
